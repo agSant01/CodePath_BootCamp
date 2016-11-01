15c3fe776dd51de4e373f9f1053ff4543bd29e8f
Gabriel S. Santiago <gabriel.santiago16@upr.edu>
2016-11-01 08:37:07 -0400

    Determines if a string is a palindrome.

--- Week_1/Challenge_1/src/DuplicateRemover.java
+++ Week_1/Challenge_1/src/DuplicateRemover.java
@@ -5,30 +5,28 @@
  * Created by Gabriel S. Santiago on 10/25/16.
  * Copyright © 2016 Gabriel S. Santiago. All rights reserved.
  *
-**/
+ **/
 
 public class DuplicateRemover {
-	
 	public static int duplicateRemover(int[] sortedList){
 		int arrayLength = sortedList.length;
 		int index;
-		
+
 		if(arrayLength <= 1 || sortedList == null){
 			return arrayLength;
 		}
-
 		for (int i = 0; i < arrayLength ; i ++){
 			index = i + 1;
 			//Verifies if index is in the array bounds
 			//If selected number is equal to the index:
-			//	I. Set index as 0
+			//		I. Set index as 0
 			//  II. Move to next index
 			while(index < arrayLength && sortedList[i] == sortedList[index]){
 				sortedList[index] = 0;
 				index++;
 			}
 		}
-		
+
 		//Travels through the array to move 0's to the right end
 		for(int i = 0; i < arrayLength; i++){
 			index = i;
@@ -46,12 +44,10 @@ public class DuplicateRemover {
 				sortedList[index] = 0;
 			}
 		}
-		
 		index = 0;
 		while(sortedList[index] != 0){
 			index++;
 		}
-		
 		return index;
 	}
 }
